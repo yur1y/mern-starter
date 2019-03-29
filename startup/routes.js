c8fcb89ec2560e9505d5d3c6f8414e729262b6cd
Yuri <yuriyiskiv@gmail.com>
2019-03-29 16:05:27 +0200

    server posts routes

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const error = require('../middleware/error');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const posts =require('../routes/post')
 const cors = require('cors');
 
 module.exports = (app) => {
@@ -9,5 +10,6 @@ module.exports = (app) => {
     app.use(express.json());
      app.use('/api/users', users);
     app.use('/api/auth', auth);
+    app.use('/api/posts',posts)
     app.use(error);
 }
