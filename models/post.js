23e1154f0d95340afcf9e4523bf57392570d0b0f
Yuri <yuriyiskiv@gmail.com>
2019-03-29 16:02:46 +0200

    server post

---
+++ models/post.js
@@ -0,0 +1,44 @@
+
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+        unique: true
+    },
+    content:
+        {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 8192
+        },
+    slug:{
+        type: String,
+        required:true
+    },
+    createdAt: {
+        type: Date,
+        required:true,
+        default: new Date.now
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+function validatePost(post) {
+    const schema = {
+        title: Joi.string().min(5).max(50).required(),
+        content:Joi.string().min(1).max(8192).required()
+    };
+
+    return Joi.validate(post, schema);
+}
+
+exports.postSchema = postSchema;
+exports.Post= Post;
+exports.validate = validatePost;
---
+++ routes/post.js
@@ -0,0 +1,64 @@
+const validateObjectId = require('../middleware/validateObjectId');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+
+const {Post, validate} = require('../models/post');
+const express = require('express');
+const router = express.Router();
+import slug from 'limax';
+import sanitizeHtml from 'sanitize-html';
+
+router.get('/', async (req, res) => {
+    const post = await Post.find().sort('createdAt');
+    res.send(post);
+});
+
+router.get('/:id', [auth, validateObjectId], async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).send('The post with the given ID was not found.');
+
+    res.send(post);
+});
+
+router.post('/', auth, async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const {name,content} =req.body;
+    let post = await Post.findOne({name});
+
+    if (post) return res.status(400).send('Post with the same name already exist.');
+
+    post = new Post({name:sanitizeHtml(name), content:sanitizeHtml(content), slug:slug(name.toLowerCase(), { lowercase: true })});
+    post = await post.save();
+
+    res.send(post);
+});
+
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const {name,content} = req.body;
+    const post = await Post.findByIdAndUpdate(req.params.id, {name, content}, {
+        //return updated post object
+        new: true
+    });
+
+    if (!post) return res.status(404).send('The puppet with the given ID was not found.');
+
+    res.send(post);
+});
+
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
+    const post = await Post.findByIdAndRemove(req.params.id);
+
+    if (!post) return res.status(404).send('The post with the given ID was not found.');
+
+    res.send(post);
+});
+
+
+
+
+module.exports = router;
